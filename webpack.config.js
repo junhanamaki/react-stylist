dccd02ffea1c1c9dc9bf47d6c3a487bc21adc135
junhanamaki <jun.hanamaki@gmail.com>
2015-12-20 13:52:26 +0000

    study

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
+var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var MINIFIED = JSON.parse(process.env.MINIFIED || '0');
 
 module.exports = {
   entry: './example/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'react-stylist.js'
+    filename: MINIFIED ? 'react-stylist.min.js' : 'react-stylist.js'
   },
   module: {
     preLoaders: [{
@@ -21,9 +24,12 @@ module.exports = {
       loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]')
     }]
   },
-  plugins: [
-    new ExtractTextPlugin("react-stylist.css")
-  ],
+  plugins: MINIFIED
+    ? [
+        new ExtractTextPlugin("react-stylist.min.css"),
+        new webpack.optimize.UglifyJsPlugin({ minimize: true })
+      ]
+    : [new ExtractTextPlugin("react-stylist.css")],
   eslint: {
     configFile: path.resolve(__dirname, '.eslintrc')
   }
